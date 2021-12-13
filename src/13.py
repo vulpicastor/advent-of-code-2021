7ce017262909bee0823c1eb4093cdafca536c12a
Lizhou Sha <lizhou@lsha.net>
2021-12-13 00:12:33 -0600

    Day 13 cleanup

--- src/13.py
+++ src/13.py
@@ -15,8 +15,10 @@ import aocd
 YEAR = 2021
 DAY = 13
 
-
+AXIS_MAP = {'x': 0, 'y': 1}
 PAPER_FMT = ['.', '#']
+
+
 def paper_str(p):
     real_p = p.T
     lines = []
@@ -24,13 +26,14 @@ def paper_str(p):
         lines.append(''.join(PAPER_FMT[int(i)] for i in row))
     return '\n'.join(lines)
 
+
 def index(s, axis):
     out = [slice(None, None, None)] * 2
     out[axis] = s
     return tuple(out)
 
+
 def origami(a, axis, fold):
-    # print(a.shape, axis)
     len_edge = a.shape[axis]
     recto_end = fold
     verso_start = fold + 1
@@ -40,24 +43,14 @@ def origami(a, axis, fold):
 
     if len_recto >= len_verso:
         out = a[index(slice(0, recto_end), axis)].copy()
-        # print(paper_str(out[index(slice(-len_verso, None), axis)]))
         out[index(slice(-len_verso, None), axis)] |= a[
             index(slice(len_edge, verso_start-1, -1), axis)]
     else:
         out = a[index(slice(verso_start, None), axis)].copy()
         out[index(slice(len_recto), axis)] |= a[
             index(slice(len_recto, -1, -1), axis)]
-
-    # if pos >= len_verso:
-    #     slice_recto_part = slice(fold-1, len_fold-1, -1)
-    #     out[index(slice_recto_part, axis)] |= a[index(slice_verso, axis)]
-    # else:
-    #     slice_verso_part = slice(len_verso-1, -1, -1)
-    #     out = a[index(slice_verso, axis)].copy()
-    #     out[index(slice_verso_part, axis)] |= a[index(slice_verso, axis)]
     return out
 
-AXIS_MAP = {'x': 0, 'y': 1}
 
 def main():
     data = """6,10
@@ -94,22 +87,15 @@ fold along x=5"""
         a, p = s.split('=')
         arg_list.append((AXIS_MAP[a], int(p)))
 
-    # print(paper_str(paper))
-    # print()
     out = origami(paper, *arg_list[0])
     answer = np.sum(out)
     print(answer)
-    # aocd.submit(answer, part='a', day=DAY, year=YEAR)
-
+    aocd.submit(answer, part='a', day=DAY, year=YEAR)
 
     out = paper
     for args in arg_list:
         out = origami(out, *args)
-
-    print(paper_str(out), '\n')
-
-
-    # aocd.submit(answer, part='b', day=DAY, year=YEAR)
+    print(paper_str(out))
 
 
 if __name__ == '__main__':
