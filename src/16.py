78b9c283a016e12acbfdd632b2cdf11d9ed27bbe
Lizhou Sha <lizhou@lsha.net>
2021-12-16 01:18:56 -0600

    Day 16 cleanup

--- src/16.py
+++ src/16.py
@@ -17,11 +17,9 @@ YEAR = 2021
 DAY = 16
 
 
-def hex2bin(c):
-    return format(int(c, base=16), '04b')
-
 LiteralPacket = collections.namedtuple(
     'LiteralPacket', ['version', 'type_id', 'value'])
+
 
 OpPacket = collections.namedtuple(
     'OpPacket', ['version', 'type_id', 'len_type_id', 'length', 'subpackets'])
@@ -33,45 +31,37 @@ def parse_literal(strio, pos):
         do_continue = int(strio.read(1))
         nibbles.append(strio.read(4))
         pos += 5
-        # print(nibbles, do_continue, pos)
         if not do_continue:
             break
-    # print(nibbles)
-    # if ignore_padding:
-    #     padding = strio.read(4 - (pos % 4))
-    #     print(padding)
-    #     if len(padding) > 0 and not all(c == '0' for c in padding):
-    #         raise ValueError('Non-zero padding in literal packet')
     value = int(''.join(nibbles), base=2)
     return value, pos
+
 
 def parse_operator(strio, pos):
     len_type = int(strio.read(1))
     pos += 1
     version_sum = 0
-    match len_type:
-        case 0:
-            num_bits = int(strio.read(15), base=2)
-            pos += 15
-            # print('   ', len_type, num_bits, pos)
-            old_pos = pos
-            subpackets = []
-            while pos - old_pos < num_bits-1:
-                packet, pos, add_version = parse_packet(strio, pos)
-                version_sum += add_version
-                subpackets.append(packet)
-            return len_type, num_bits, subpackets, pos, version_sum
-        case 1:
-            num_packs = int(strio.read(11), base=2)
-            pos += 11
-            # print('   ', len_type, num_packs, pos)
-            subpackets = []
-            for _ in range(num_packs):
-                packet, pos, add_version = parse_packet(strio, pos)
-                version_sum += add_version
-                subpackets.append(packet)
-            return len_type, num_packs, subpackets, pos, version_sum
-    return None, None, None, None
+    if len_type == 0:
+        num_bits = int(strio.read(15), base=2)
+        pos += 15
+        old_pos = pos
+        subpackets = []
+        while pos - old_pos < num_bits-1:
+            packet, pos, add_version = parse_packet(strio, pos)
+            version_sum += add_version
+            subpackets.append(packet)
+        return len_type, num_bits, subpackets, pos, version_sum
+    if len_type == 1:
+        num_packs = int(strio.read(11), base=2)
+        pos += 11
+        subpackets = []
+        for _ in range(num_packs):
+            packet, pos, add_version = parse_packet(strio, pos)
+            version_sum += add_version
+            subpackets.append(packet)
+        return len_type, num_packs, subpackets, pos, version_sum
+    raise ValueError('Unknown operator packet length type')
+
 
 def parse_packet(strio, pos):
     version_str = strio.read(3)
@@ -81,37 +71,27 @@ def parse_packet(strio, pos):
     pos += 3
     type_id = int(strio.read(3), base=2)
     pos += 3
-    # print(version, type_id)
     match type_id:
         case 4:
             value, pos = parse_literal(strio, pos)
-            # print(pos)
             return LiteralPacket(version=version, type_id=4, value=value), pos, version
         case _:
             len_type, length, subpackets, pos, add_version = parse_operator(strio, pos)
-            # print(pos)
             return OpPacket(
                 version=version, type_id=type_id, len_type_id=len_type,
                 length=length, subpackets=subpackets), pos, version + add_version
 
 
+def hex2bin(c):
+    return format(int(c, base=16), '04b')
+
 
 def parse(s):
-    # packets = []
     bit_str = ''.join(map(hex2bin, s))
-    # print(' '.join(map(hex2bin, s)))
     with io.StringIO(bit_str) as strio:
-        # print(strio.write(bit_str), 4*len(s))
-        # strio.flush()
-        # print(strio.read())
         pos = 0
         return parse_packet(strio, pos)
-        # while True:
-            # packet, pos = parse_packet(strio, pos, True)
-            # if packet is None:
-                # break
-            # packets.append(packet)
-    # return packets
+
 
 def calc(packet):
     match packet.type_id:
@@ -126,13 +106,10 @@ def calc(packet):
         case 3:
             return max(map(calc, packet.subpackets))
         case 5:
-            print(f'{calc(packet.subpackets[0])} > {calc(packet.subpackets[1])}')
             return calc(packet.subpackets[0]) > calc(packet.subpackets[1])
         case 6:
-            print(f'{calc(packet.subpackets[0])} < {calc(packet.subpackets[1])}')
             return calc(packet.subpackets[0]) < calc(packet.subpackets[1])
         case 7:
-            print(f'{calc(packet.subpackets[0])} == {calc(packet.subpackets[1])}')
             return calc(packet.subpackets[0]) == calc(packet.subpackets[1])
         case _:
             raise ValueError('Unknown packet type_id')
@@ -148,17 +125,9 @@ def main():
         'C0015000016115A2E0802F182340',
         'A0016C880162017C3686B18A3D4780',
     ]
-    data = aocd.get_data(day=DAY, year=YEAR)
-    # inlist = data.split('\n')
-    # for p in test_packets:
-    #     print(p)
-    #     print(parse(p), '\n')
-    parsed_packet, bits_read, version_sum = parse(data)
-    print(parsed_packet)
-    print(version_sum)
-    answer = version_sum
-
-    # aocd.submit(answer, part='a', day=DAY, year=YEAR)
+    for p in test_packets:
+        print(p)
+        print(parse(p), '\n')
 
     test_packets_2 = [
         'C200B40A82',
@@ -170,16 +139,23 @@ def main():
         '9C005AC2F8F0',
         '9C0141080250320F1802104A08',
     ]
-    # for p in test_packets_2:
-    #     print(p)
-    #     pp, _, _ = parse(p)
-    #     print(pp)
-    #     print(calc(pp))
-    #     print()
-    
+    for p in test_packets_2:
+        print(p)
+        pp, _, _ = parse(p)
+        print(pp)
+        print(calc(pp))
+        print()
+
+    data = aocd.get_data(day=DAY, year=YEAR)
+
+    parsed_packet, _, version_sum = parse(data)
+    print(parsed_packet)
+    answer = version_sum
+    print(answer)
+    aocd.submit(answer, part='a', day=DAY, year=YEAR)
+
     answer = calc(parsed_packet)
     print(answer)
-
     aocd.submit(answer, part='b', day=DAY, year=YEAR)
 
 
