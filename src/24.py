a78c78835883bcd815a12daf3c977c653a89bef7
Lizhou Sha <lizhou@lsha.net>
2021-12-24 06:19:21 -0600

    Day 24: Carnage of Python solution, this one takes years

--- src/24.py
+++ src/24.py
@@ -73,10 +73,10 @@ def compile(instlist):
                 func = lambda x, y: int(x == y)
         if b in 'wxyz':
             def step(regs, myfunc=func, reg_a=REG_MAP[a], reg_b=REG_MAP[b]):
-                regs[reg_a] = func(regs[reg_a], regs[reg_b])
+                regs[reg_a] = myfunc(regs[reg_a], regs[reg_b])
         else:
             def step(regs, myfunc=func, reg_a=REG_MAP[a], val_b=int(b)):
-                regs[reg_a] = func(regs[reg_a], val_b)
+                regs[reg_a] = myfunc(regs[reg_a], val_b)
         funclist.append((1, step))
     return funclist
 
@@ -90,15 +90,67 @@ def run_funclist(funclist, inputs):
         regs[func] = next(input_iter)
     return regs
 
+def compile_simple(instlist):
+    funclist = []
+    for inst in instlist:
+        if not inst:
+            continue
+        cmd, a, b = inst.split()
+        match cmd:
+            case 'add':
+                func = op.add
+            case 'mul':
+                func = op.mul
+            case 'div':
+                func = op.floordiv
+            case 'mod':
+                func = op.mod
+            case 'eql':
+                func = lambda x, y: int(x == y)
+        if b in 'wxyz':
+            def step(regs, myfunc=func, reg_a=REG_MAP[a], reg_b=REG_MAP[b]):
+                regs[reg_a] = myfunc(regs[reg_a], regs[reg_b])
+        else:
+            def step(regs, myfunc=func, reg_a=REG_MAP[a], val_b=int(b)):
+                regs[reg_a] = myfunc(regs[reg_a], val_b)
+        funclist.append(step)
+    return funclist
 
+def run_simple(funclist, n):
+    regs = [n, 0, 0, 0]
+    for step in funclist:
+        step(regs)
+    return regs
 
 def main():
     data = aocd.get_data(day=DAY, year=YEAR)
+    inblocks = data.split('inp w\n')
+    print(inblocks)
+    blocks = [compile_simple(l.split('\n')) for l in inblocks if l]
+    max_digits = []
+    for funclist in blocks:
+        next_digit = None
+        for i in range(9, 0, -1):
+            print(funclist)
+            outregs = run_simple(funclist, i)
+            if outregs[3] == 0:
+                next_digit = i
+                break
+        if next_digit is None:
+            raise ValueError()
+        max_digits.append(next_digit)
+    print(max_digits)
+
+    return
     inlist = data.split('\n')
 
     funclist = compile(inlist)
+    print(funclist)
+
     for i in tqdm.tqdm(range(99999999999999, 9999999999999, -1)):
-        # print(list(map(int, str(i))))
+        digits = list(map(int, str(i)))
+        if any(d == 0 for d in digits):
+            continue
         outregs = run_funclist(funclist, list(map(int, str(i))))
         if outregs[3] == 0:
             answer = i
